9651db96b1897c531357be7142cb702aa9dbe629
unknown <lorenzoquaroni24@gmail.com>
2025-07-27 22:56:53 +0200

    Collegamento con il Database (PostGre / pgAdmin 4)

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,21 @@
 package com.example.demo.student;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
+@Entity
+@Table
 
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "studente_sequence",
+            sequenceName = "studente_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
